d16dbf08ec07b1133af44ba957bd8e7d4d9a7c95
Fabrizio Bottaro <fabrizio5680@gmail.com>
2015-09-24 19:25:13 +0100

    datatables

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 if (!console.error) {
-  console.error = console.log;
+    console.error = console.log;
 }
 'use strict';
 
@@ -13,80 +13,92 @@ if (!console.error) {
  */
 angular
     .module('surveyTreeModuleApp', [
-      'ngAnimate',
-      'ngTouch',
-      'ngRoute',
-      'LocalStorageModule',
-      'angularSpinner',
-      'eydis.gapi'
+        'ngAnimate',
+        'ngTouch',
+        'ngRoute',
+        'ngResource',
+        'ngMaterial',
+        'datatables',
+        'LocalStorageModule',
+        'angularSpinner',
+        'eydis.gapi'
     ])
 
 
-    .config(function ($gapiProvider, localStorageServiceProvider, $locationProvider, $routeProvider) {
-      $gapiProvider.api_base = 'https://api-dot-vivosme.appspot.com/_ah/api';
+    .config(function ($gapiProvider, localStorageServiceProvider, $locationProvider, $routeProvider, $mdThemingProvider) {
+        $gapiProvider.api_base = 'https://api-dot-vivosme.appspot.com/_ah/api';
 
-      localStorageServiceProvider.setPrefix('vivos.admin');
 
-      $routeProvider
-          .when('/results/:questionnaireId', {
-            templateUrl: 'views/results.html',
-            controller: 'ResultsCtrl',
-            controllerAs: 'results'
-          })
-          .when('/candidates', {
-            templateUrl: 'views/table.html',
-            controller: 'MainCtrl',
-            reloadOnSearch: false
-          })
-          .when('/register', {
-            templateUrl: 'views/register.html',
-            controller: 'RegisterCtrl',
-            controllerAs: 'register'
-          })
-          .when('/welcome', {
-            templateUrl: 'views/welcome.html',
-            controller: 'WelcomeCtrl',
-            controllerAs: 'welcome'
-          })
-          .when('/profile/:profileId', {
-            templateUrl: 'views/profile.html',
-            controller: 'ProfileCtrl',
-            controllerAs: 'profile'
-          })
-          .otherwise({
-            redirectTo: '/register'
-          });
+        $mdThemingProvider.theme('default')
+            .primaryPalette('amber')
+            .accentPalette('grey');
+
+        localStorageServiceProvider.setPrefix('vivos.admin');
+
+        $routeProvider
+            .when('/results/:questionnaireId', {
+                templateUrl: 'views/results.html',
+                controller: 'ResultsCtrl',
+                controllerAs: 'results'
+            })
+            .when('/candidates', {
+                templateUrl: 'views/table.html',
+                controller: 'MainCtrl',
+                reloadOnSearch: false
+            })
+            .when('/register', {
+                templateUrl: 'views/register.html',
+                controller: 'RegisterCtrl',
+                controllerAs: 'register'
+            })
+            .when('/welcome', {
+                templateUrl: 'views/welcome.html',
+                controller: 'WelcomeCtrl',
+                controllerAs: 'welcome'
+            })
+            .when('/profile/:profileId', {
+                templateUrl: 'views/profile.html',
+                controller: 'ProfileCtrl',
+                controllerAs: 'profile'
+            })
+            .otherwise({
+                redirectTo: '/register'
+            });
     })
 
 
     .run(function ($rootScope, $location, localStorageService, api, usSpinnerService) {
 
-      $rootScope.logout = function () {
-        localStorageService.set('user', null);
-        $location.url('/register');
-        api.setToken(null);
-      };
+        $rootScope.logout = function () {
+            localStorageService.set('user', null);
+            $location.url('/register');
+            api.setToken(null);
+        };
+
+        $rootScope.toggleSidenav = function(menuId) {
+            $mdSidenav(menuId).toggle();
+        };
 
 
-      $rootScope.startSpin = function(){
-        usSpinnerService.spin('main');
-      };
-      $rootScope.stopSpin = function(){
-        usSpinnerService.stop('main');
-      };
+        $rootScope.startSpin = function(){
+            usSpinnerService.spin('main');
+        };
+        $rootScope.stopSpin = function(){
+            usSpinnerService.stop('main');
+        };
 
 
-      var init = function () {
-        try {
-          var user = localStorageService.get('user');
-          api.setToken(user.authToken);
-        } catch (ignore){
-          $location.url('/');
-        }
+        var init = function () {
+            try {
+                var user = localStorageService.get('user');
+                api.setToken(user.authToken);
+            } catch (ignore){
+                $location.url('/');
+            }
 
-        window.webshims.polyfill();
-      };
+            window.webshims.polyfill();
+        };
 
-      init();
+        init();
 
     });
